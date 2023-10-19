1da0417a2713dfc4ba2956da04090ff7b8d7e847
LeoDm123 <leo.meiners1@gmail.com>
2023-10-19 12:25:39 -0300

    cambio en componentes

--- src/js/App.js
+++ src/js/App.js
@@ -1,40 +1,31 @@
 import { useLocation } from "react-router-dom";
 
-const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-   event.preventDefault(); // Prevent the default form submission behavior
+const handleSubmit = (event) => {
+  event.preventDefault(); // Prevent the default form submission behavior
 
-   // Retrieve the form values using the "name" attribute
-   const userName = (
-     event.currentTarget.elements.namedItem("userInput") as HTMLInputElement
-   ).value;
-   const email = (
-     event.currentTarget.elements.namedItem("emailInput") as HTMLInputElement
-   ).value;
-   const password = (
-     event.currentTarget.elements.namedItem(
-       "passwordInput"
-     ) as HTMLInputElement
-   ).value;
-   const passwordConfirmation = (
-     event.currentTarget.elements.namedItem(
-       "passwordConfirmationInput"
-     ) as HTMLInputElement
-   ).value;
+  // Retrieve the form values using the "name" attribute
+  const userName = event.currentTarget.elements.namedItem("userInput").value;
+  const email = event.currentTarget.elements.namedItem("emailInput").value;
+  const password =
+    event.currentTarget.elements.namedItem("passwordInput").value;
+  const passwordConfirmation = event.currentTarget.elements.namedItem(
+    "passwordConfirmationInput"
+  ).value;
 
-   // Create a user object to save in local storage
-   const user = {
-     userName,
-     email,
-     password,
-     passwordConfirmation,
-   };
+  // Create a user object to save in local storage
+  const user = {
+    userName,
+    email,
+    password,
+    passwordConfirmation,
+  };
 
-   // Save the user object to local storage
-   localStorage.setItem("user", JSON.stringify(user));
+  // Save the user object to local storage
+  localStorage.setItem("user", JSON.stringify(user));
 
-   // Clear the form after submission (optional)
-   event.currentTarget.reset();
-   window.location.href = "/";
- };
+  // Clear the form after submission (optional)
+  event.currentTarget.reset();
+  window.location.href = "/";
+};
 
 export default handleSubmit;
