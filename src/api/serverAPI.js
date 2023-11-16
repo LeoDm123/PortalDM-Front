3d5a317e36d704990e6d1e09433927523b527e07
LeoDm123 <leo.meiners1@gmail.com>
2023-11-16 14:23:12 -0300

    cambio de URL

--- src/api/serverAPI.js
+++ src/api/serverAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const serverAPI = axios.create({
-  baseURL: "http://localhost:4040",
+  // baseURL: "http://localhost:4040",
+  baseURL: "https://portaldm-api.onrender.com",
 });
 
 export default serverAPI;
